1b61dc10c3a3baee06e1ee0bb59d05ce1099dd08
Sobcat <771954909@qq.com>
2019-12-06 11:43:42 +0800

    模仿jq选择器原理

--- simplify_jquery.js
+++ simplify_jquery.js
@@ -2,21 +2,49 @@
 *
 * 造别人的轮子, 开自己的车
 * JavaScript 练习
-* 简化版的 JQuery
+* 简化版的 JQuery 选择器原理
 *
 */
 
 ;(function(){
-    var $ = function(){
-        return new Base();
+    var $ = function(str){
+        var base = new Base();
+        if(str[0]=="#"){
+            base.$(str.substr(1,str.length-1));
+        }else if(str[0]=="."){
+            base.$$(str.substr(1,str.length-1));
+        }else{
+
+        }
+        return base;
     }
     function Base() {
+        this.elements = []; //创建数组,保存获取的节点和节点数组
+        this.current = -1; //用于遍历
         this.$ = function(id){
-            return document.getElementById(id);
+            this.elements.push(document.getElementById(id));
+            return this; //返回对象 不然无法连缀
         };
         this.$$ = function(calssName){
-            return document.getElementsByClassName(calssName);
-        }
-        this.css = function(){}
+            var clss = document.getElementsByClassName(calssName);
+            for(var i=0;i<clss.length;i++){
+                this.elements.push(clss[i]);
+            }
+            return this;
+        };
+        this.eq = function(index) {
+            this.current = index;
+            return this;
+        };
+        this.css = function(attr, val){
+            if(this.current !=-1 ){
+                this.elements[this.current].style[attr] = val;
+            }else {
+                for(var i=0;i<this.elements.length;i++){
+                    this.elements[i].style[attr] = val;
+                }
+            }
+            return this;
+        };
     }
 })();
